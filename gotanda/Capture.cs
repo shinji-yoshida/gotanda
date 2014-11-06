31a18d703b3ac30271e2a6ac7e5a347862c182fc
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2014-11-06 14:44:14 +0900

    Capture and TransformExtension

---
+++ gotanda/Capture.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace gotanda{
+	/// usage
+	/// 
+	/// intarface Foo{
+	///   void Bar(int n);
+	/// }
+	/// 
+	/// var capture = new Capture<int>();
+	/// var mockFoo = new Mock<Foo>();
+	/// mockFoo.Setup(f => f.Bar(It.IsAny<int>()).Callback<int>(capture);
+	/// mockFoo.Object.Bar(3);
+	/// Assert.Equals(3, capture.Arg1);
+	/// 
+	public class Capture<T1>{
+		T1 arg1;
+		
+		public T1 Arg1 {
+			get {
+				return arg1;
+			}
+		}
+		
+		public Action<T1> Callback(){
+			return (arg) => {this.arg1 = arg;};
+		}
+		
+		public static implicit operator Action<T1>(Capture<T1> c){
+			return c.Callback();
+		}
+	}
+}
---
+++ gotanda/TransformExtension.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace gotanda{
+	public static class TransformExtension {
+		public static void SetLocalScale2D(this Transform transform, float x, float y){
+			transform.localScale = new Vector3(x, y, 1);
+		}
+
+		public static void SetLocalScale2D(this Transform transform, Vector2 scale){
+			transform.localScale = new Vector3(scale.x, scale.y, 1);
+		}
+	}
+}
