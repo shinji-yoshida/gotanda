3d9d317564efc1a93d76a1bfbe24873584b39802
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2014-11-06 20:20:42 +0900

    TransformExtension

--- gotanda/TransformExtension.cs
+++ gotanda/TransformExtension.cs
@@ -10,5 +10,9 @@ namespace gotanda{
 		public static void SetLocalScale2D(this Transform transform, Vector2 scale){
 			transform.localScale = new Vector3(scale.x, scale.y, 1);
 		}
+		
+		public static void SetPositionY(this Transform transform, float y){
+			transform.position = new Vector3(transform.position.x, y, transform.position.z);
+		}
 	}
 }
