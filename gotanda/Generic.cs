0194ebe1d52596f1bf2bcafabba37e631d4e28a5
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2014-11-11 16:09:36 +0900

    Generic#Tap

---
+++ gotanda/Generic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace gotanda{
+	public static class Generic {
+		public static T Tap<T>(this T self, Action<T> action){
+			action(self);
+			return self;
+		}
+	}
+}
+	
