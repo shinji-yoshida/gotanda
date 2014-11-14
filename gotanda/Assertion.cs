b8ea9bfa1315c6986944669f0187df02566edbbf
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2014-11-14 10:19:09 +0900

    Assertion

---
+++ gotanda/Assertion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace gotanda{
+	public static class Assertion {
+		public static void _assert_<T>(this T dammy, bool condition){
+#if DEBUG
+			if(condition)
+				return;
+			throw new UnityException("assertion failure");
+#endif
+		}
+
+		public static void _assert_(bool condition){
+#if DEBUG
+			if(condition)
+				return;
+			throw new UnityException("assertion failure");
+#endif
+		}
+	}
+}
