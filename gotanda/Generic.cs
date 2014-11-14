006ca7695f35994f4de2e617491ebe3789c41e8e
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2014-11-14 19:05:01 +0900

    Generic#Swap

--- gotanda/Generic.cs
+++ gotanda/Generic.cs
@@ -8,6 +8,13 @@ namespace gotanda{
 			action(self);
 			return self;
 		}
+
+		public static void Swap<T>(ref T lhs, ref T rhs){
+			T temp;
+			temp = lhs;
+			lhs = rhs;
+			rhs = temp;
+		}
 	}
 }
 	
