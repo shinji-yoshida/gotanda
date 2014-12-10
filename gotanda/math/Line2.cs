d43317cbf00111d094fe1d550f57713497935114
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2014-12-10 20:47:19 +0900

    Line2 and Vector2Math

---
+++ gotanda/math/Line2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+namespace gotanda{
+	public class Line2{
+		Vector2 basePoint;
+		Vector2 direction;
+		
+		public Vector2 BasePoint {
+			get {
+				return basePoint;
+			}
+		}
+		
+		public Vector2 Direction {
+			get {
+				return direction;
+			}
+		}
+		
+		public Line2 (Vector2 basePoint, Vector2 direction)
+		{
+			Assertion._assert_(Mathf.Abs(direction.magnitude - 1) < 0.000001);
+			
+			this.basePoint = basePoint;
+			this.direction = direction;
+		}
+		
+		public Vector2 PointFromBasePoint(float length){
+			return basePoint + direction * length;
+		}
+	}
+}
