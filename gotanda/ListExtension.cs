bfcac03fce43b159222c58b01046a69cb56ad627
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-05-14 16:04:54 +0900

    change IListExtension to ListExtension

--- gotanda/ListExtension.cs
+++ gotanda/ListExtension.cs
@@ -3,25 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace gotanda{
-	public static class IListExtension {
-		public static bool IsEmpty<T>(this IList<T> list){
+	public static class ListExtension {
+		public static bool IsEmpty<T>(this List<T> list) {
 			return list.Count == 0;
 		}
 		
-		public static T First<T>(this IList<T> list){
+		public static T First<T>(this List<T> list) {
 			return list[0];
 		}
 		
-		public static T Last<T>(this IList<T> list){
+		public static T Last<T>(this List<T> list) {
 			return list[list.Count - 1];
 		}
 
-		public static T Sample<T>(this IList<T> list) {
+		public static T Sample<T>(this List<T> list) {
 			Assertion._assert_(! list.IsEmpty());
 			return list[Random.Range(0, list.Count)];
 		}
 
-		public static IList<T> Copy<T>(this IList<T> list){
+		public static List<T> Copy<T>(this List<T> list) {
 			return new List<T>(list);
 		}
 	}
